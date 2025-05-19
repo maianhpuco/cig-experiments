de9d56329634b7a4f814274dd0fcd145c287062d
Mai Anh Vu <maianhuel@gmail.com>
2025-05-18 22:53:25 -0500

    update

--- ig_clam.py
+++ ig_clam.py
@@ -114,7 +114,7 @@ def main(args):
             "call_model_function": call_model_function,
             "model": model,
             "baseline_features": stacked_features_baseline,
-            "memmap_path": args.paths['mempath'],
+            "memmap_path": args.paths['memmap_path'],
             "x_steps": 50,
         }
 
