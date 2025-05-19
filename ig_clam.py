288969edeae22440d96b8231a97aabbe4fc2df03
Mai Anh Vu <maianhuel@gmail.com>
2025-05-18 19:05:35 -0500

    update

--- ig_clam.py
+++ ig_clam.py
@@ -17,22 +17,16 @@ sys.path.append(ig_path)
 sys.path.append(clf_path)  
 
 from clam import load_clam_model  
-# from attr_method._common import (
-#     sample_random_features,
-#     call_model_function
-# ) 
+from attr_method._common import (
+    sample_random_features,
+    call_model_function
+) 
 # from data.ig_dataset import IG_dataset
 # from utils.utils import load_pkl
 # from utils.models.model_utils import get_clam_model
 # from saliency.core.base import CoreSaliency
  
-# # base_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")) 
-
-
-
-
-
-
+# # base_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../"))
 # from LoadClamModel import load_clam_model  # adjust if the function is in a different file
 
 def get_dummy_args():
