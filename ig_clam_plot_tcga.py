7a3f913abafcf6a62f8f34a48f294298fa80213d
Mai Anh Vu <maianhuel@gmail.com>
2025-05-22 12:12:15 -0500

    update

--- ig_clam_plot_tcga.py
+++ ig_clam_plot_tcga.py
@@ -35,7 +35,9 @@ def build_slide_path_mapping(split_csv_path):
 
 def find_slide_path_mapping(basename, slide_path):
     slide_path = "/home/mvu9/datasets/TCGA-datasets"
+    
     paths = glob.glob(os.path.join(slide_path, f"*/*/{basename}.svs"))
+    print("Paths: ", paths)
     return paths[0] if paths else None
     
 
