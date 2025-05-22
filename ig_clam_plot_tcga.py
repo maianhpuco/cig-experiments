4e3d6c1f2f9a18724e41aba6fa8e02afe199ff7a
Mai Anh Vu <maianhuel@gmail.com>
2025-05-22 14:23:23 -0500

    update

--- ig_clam_plot_tcga.py
+++ ig_clam_plot_tcga.py
@@ -144,7 +144,7 @@ if __name__ == '__main__':
 
     args.device = "cuda" if torch.cuda.is_available() else "cpu"
     config = load_config(args.config) 
-    # main(args, config)
+    main(args, config)
     
     # missing_files = ['TCGA-A3-3313-01Z-00-DX1.10f8cd5e-1c4f-4bb4-96f3-c612f093fc80']
     # slide_path = '/home/mvu9/datasets/TCGA-datasets/'
