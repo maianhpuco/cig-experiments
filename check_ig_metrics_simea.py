e4109dc561185c4b9f03ef892b1eb4f741f061aa
Mai Anh Vu <maianhuel@gmail.com>
2025-06-01 19:17:00 -0500

    update

--- check_ig_metrics_simea.py
+++ check_ig_metrics_simea.py
@@ -146,7 +146,8 @@ def main(args, config):
         use_h5=True
     ) 
     # For example, if features_data is [N, D], match shape
-        stacked_features_baseline = sample_random_features(test_dataset).to(args.device, dtype=torch.float32)
+    stacked_features_baseline = sample_random_features(test_dataset).to(args.device, dtype=torch.float32)
+    
     if features_data.dim() == 2:
         num_patches = features_data.shape[0]
         sampled_indices = np.random.choice(stacked_features_baseline.shape[0], size=num_patches, replace=True)
